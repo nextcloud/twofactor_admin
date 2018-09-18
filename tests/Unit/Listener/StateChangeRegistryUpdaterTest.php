55cdc0cffa44209becae9aa9d73b4b4b7f3ac50a
Christoph Wurst <christoph@winzerhof-wurst.at>
2018-09-18 10:49:03 +0200

    Add phpunit config and a first test class
    Signed-off-by: Christoph Wurst <christoph@winzerhof-wurst.at>

---
+++ tests/Unit/Listener/StateChangeRegistryUpdaterTest.php
@@ -0,0 +1,84 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\TwoFactorAdmin\Test\Unit\Listener;
+
+use ChristophWurst\Nextcloud\Testing\TestCase;
+use OCA\TwoFactorAdmin\Event\StateChanged;
+use OCA\TwoFactorAdmin\Listener\StateChangeRegistryUpdater;
+use OCA\TwoFactorAdmin\Provider\AdminProvider;
+use OCA\TwoFactorGateway\Service\Gateway\SMS\Provider\IProvider;
+use OCP\Authentication\TwoFactorAuth\IRegistry;
+use OCP\IUser;
+use Symfony\Component\EventDispatcher\Event;
+
+class StateChangeRegistryUpdaterTest extends TestCase {
+
+	/** @var IProvider */
+	private $registry;
+
+	/** @var AdminProvider */
+	private $provider;
+
+	/** @var StateChangeRegistryUpdater */
+	private $listener;
+
+	protected function setUp() {
+		$this->registry = $this->createMock(IRegistry::class);
+		$this->provider = $this->createMock(AdminProvider::class);
+
+		$this->listener = new StateChangeRegistryUpdater($this->registry, $this->provider);
+	}
+
+	public function testHandleUnrelatedEvent() {
+		$event = new Event();
+		$this->registry->expects($this->never())
+			->method('enableProviderFor');
+		$this->registry->expects($this->never())
+			->method('disableProviderFor');
+
+		$this->listener->handle($event);
+	}
+
+	public function testEnableEvent() {
+		$user = $this->createMock(IUser::class);
+		$event = new StateChanged($user, true);
+		$this->registry->expects($this->once())
+			->method('enableProviderFor')
+			->with($this->provider, $user);
+
+		$this->listener->handle($event);
+	}
+
+	public function testDisableEvent() {
+		$user = $this->createMock(IUser::class);
+		$event = new StateChanged($user, false);
+		$this->registry->expects($this->once())
+			->method('disableProviderFor')
+			->with($this->provider, $user);
+
+		$this->listener->handle($event);
+	}
+
+}
---
+++ tests/bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Nextcloud - U2F 2FA
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Christoph Wurst <christoph@winzerhof-wurst.at>
+ * @copyright Christoph Wurst 2016
+ */
+
+require_once __DIR__.'/../../../lib/base.php';
+require_once __DIR__.'/../vendor/autoload.php';
+
+OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
+OC_App::loadApp('twofactor_admin');
